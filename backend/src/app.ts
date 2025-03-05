50c63cb38a54b5a6f974f33c147464f0318d7727
Davy Coelho <salomao_jesus1994@outlook.com>
2025-03-05 11:52:01 -0300

    docs(routes): add swagger documentation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,14 +2,17 @@ import express from 'express';
 import { Application } from 'express';
 import * as bodyParser from 'body-parser';
 import { MainRouter } from './routes/index';
+import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
-import { expressjwt } from 'express-jwt';
 import errorHandler from './utils/errorHandler';
 const app: Application = express();
+import swaggerDocument from './swagger.json';
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api",MainRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup( swaggerDocument )
+);
 app.use(errorHandler);
 
 export default app;
